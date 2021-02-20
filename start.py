d577bd7602a9a5273bf7f417df99328769c8d362
taserbeat <taserbeat@gmail.com>
2021-02-20 22:51:14 +0900

    学習方法を改良

--- start.py
+++ start.py
@@ -9,8 +9,11 @@ import numpy as np
 
 class Game:
 
-    def __init__(self):
-        self.episode_count = 10000;
+    def __init__(self, episode_count=2000, eps=1.0, eps_min=1e-4):
+        self.episode_count = episode_count
+        self.eps = eps
+        self.eps_min = eps_min
+        self.eps_decay = (eps - eps_min) / episode_count
         # select stage
         self.env = gym.make('ppaquette/SuperMarioBros-1-1-Tiles-v0')
 
@@ -29,33 +32,53 @@ class Game:
         return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="SAME")
 
     def create_network(self, action_size):
+        s = tf.placeholder("float", [None, 13, 16, 1])
+
+        # 1層目
         W_conv1 = self.weight_variable([8, 8, 1, 16])
         b_conv1 = self.bias_variable([16])
+        h_conv1 = tf.nn.relu(self.conv2d(s, W_conv1, 2) + b_conv1)
+        h_pool1 = self.max_pool_2x2(h_conv1)
+
+        # 2層目
         W_conv2 = self.weight_variable([4, 4, 16, 32])
         b_conv2 = self.bias_variable([32])
+        h_conv2 = tf.nn.relu(self.conv2d(h_pool1, W_conv2, 1) + b_conv2)
+        h_pool2 = self.max_pool_2x2(h_conv2)
+
+        # 3層目
         W_conv3 = self.weight_variable([4, 4, 32, 64])
         b_conv3 = self.bias_variable([64])
-        W_fc1 = self.weight_variable([512, action_size])
+        h_conv3 = tf.nn.relu(self.conv2d(h_pool2, W_conv3, 1) + b_conv3)
+        h_pool3 = self.max_pool_2x2(h_conv3)
+
+        # 4層目
+        W_conv4 = self.weight_variable([4, 4, 64, 64])
+        b_conv4 = self.bias_variable([64])
+        h_conv4 = tf.nn.relu(self.conv2d(h_pool3, W_conv4, 1) + b_conv4)
+        h_pool4 = self.max_pool_2x2(h_conv4)
+
+        # 5層目
+        W_conv5 = self.weight_variable([4, 4, 64, 64])
+        b_conv5 = self.bias_variable([64])
+        h_conv5 = tf.nn.relu(self.conv2d(h_pool4, W_conv5, 1) + b_conv5)
+        h_pool5 = self.max_pool_2x2(h_conv5)
+
+        # 6層目
+        W_conv6 = self.weight_variable([4, 4, 64, 64])
+        b_conv6 = self.bias_variable([64])
+        h_conv6 = tf.nn.relu(self.conv2d(h_pool5, W_conv6, 1) * b_conv6)
+        h_pool6 = self.max_pool_2x2(h_conv6)
+
+        h_conv6_flat = tf.reshape(h_pool6, [-1, 8])
+        W_fc1 = self.weight_variable([8, action_size])
         b_fc1 = self.bias_variable([action_size])
-        s = tf.placeholder("float", [None, 13, 16, 1])
-        # hidden layers
-        h_conv1 = tf.nn.relu(self.conv2d(s, W_conv1, 2) + b_conv1)
-        h_conv2 = tf.nn.relu(self.conv2d(h_conv1, W_conv2, 1) + b_conv2)
-        h_conv3 = tf.nn.relu(self.conv2d(h_conv2, W_conv3, 1) + b_conv3)
-        h_conv3_flat = tf.reshape(h_conv3, [-1, 512])
-        readout = tf.matmul(h_conv3_flat, W_fc1) + b_fc1
+        readout = tf.matmul(h_conv6_flat, W_fc1) + b_fc1
         return s, readout
 
     def play_game(self):
-        action_list = []
         # actionは[up, left, down, right, A, B]の6種
-        for i in range(64):
-            command = format(i, 'b')
-            command = '{0:06d}'.format(int(command))
-            actions = []
-            for cmd in list(command):
-                actions.append(int(cmd))
-            action_list.append(actions)
+        action_list = [[0, 0, 0, 1, 0, 1], [0, 0, 0, 1, 1, 0], [0, 0, 0, 1, 1, 1]]
 
         sess = tf.InteractiveSession()
         s, readout = self.create_network(len(action_list))
@@ -80,12 +103,15 @@ class Game:
             distance = 0
             is_finished = False
             actions, rewards, images = [], [], []
+            self.eps = max(self.eps - self.eps_decay, self.eps_min)
             while is_finished == False:
                 screen = np.reshape(self.env.tiles, (13, 16, 1))
-                if episode < 10:
+                readout_t = readout.eval(feed_dict={s: [screen]})[0]
+
+                print("eps: {0}".format(self.eps))
+                if np.random.random() < self.eps:
                     action_index = random.randint(0, len(action_list) - 1)
                 else:
-                    readout_t = readout.eval(feed_dict={s: [screen]})[0]
                     action_index = np.argmax(readout_t)
 
                 # 画面のマリオに処理する
@@ -98,12 +124,15 @@ class Game:
 
                 # 報酬を与える
                 rewards.append([float(info['distance'])])
+
                 images.append(screen)
 
                 train_step.run(feed_dict={
                     a: actions, y: rewards, s: images
                 })
-                print('Episode : ', episode, 'Actions : ', action_list[action_index], 'Rewards', reward)
+                print("Episode: {0}, Actions: {1}, Rewards: {2}, readout_t: {3}".format(
+                    episode, action_index, rewards, readout_t)
+                )
                 actions, rewards, images = [], [], []
 
                 self.env.render()
